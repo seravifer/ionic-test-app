7da2508b2ce52c7931261f51cc636dd1192bf1ae
seravifer <sergiavilafernandez@gmail.com>
2020-06-22 18:02:04 +0200

    fix: style on iOS

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -8,7 +8,7 @@ export class ImageService {
   constructor() { }
 
   getImages(numberOfImages: number = 4000): Image[] {
-    return this.generateArrayByFn(numberOfImages, (_, index) => {
+    return this.generateArrayByFn(numberOfImages, index => {
       return {
         id : index,
         photo: this.genereteImage(index),
@@ -17,8 +17,8 @@ export class ImageService {
     });
   }
 
-  private generateArrayByFn<T>(length: number, mapFn: (v: unknown, index: number) => T): T[] {
-    return Array.from({ length }, mapFn);
+  private generateArrayByFn<T>(length: number, mapFn: (index: number) => T): T[] {
+    return Array.from({ length }, (_, i) => mapFn(i));
   }
 
   private generateText(): string {
